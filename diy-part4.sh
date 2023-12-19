61016c4a04c24a3c3377bdc26a23a15361a597c2
xktx09 <141892012+xktx11@users.noreply.github.com>
2023-12-19 10:33:05 +0800

    Update diy-part4.sh

--- diy-part4.sh
+++ diy-part4.sh
@@ -9,10 +9,10 @@
 # File name: diy-part2.sh
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
-# 修改openwrt登陆地址,把下面的192.168.110.1修改成你想要的就可以了
+# 修改openwrt登陆地址,把下面的192.168.1.1修改成你想要的就可以了
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 
-# 修改主机名字，把R30B1修改你喜欢的就行（不能纯数字或者使用中文）
+# 修改主机名字，把ASR3000修改你喜欢的就行（不能纯数字或者使用中文）
 sed -i 's/ImmortalWrt/ASR3000/g' package/base-files/files/bin/config_generate
 
 # 修改开源驱动wifi名称
